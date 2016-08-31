f4de26b948974941817443bdf6990c1de311db14
Sarah Clark <sarahclark@sarahclark-macbookair.roam.corp.google.com>
2016-08-31 16:37:36 +0100

    Simple add-to-cart

--- starting-code/app/scripts/modules/cart.js
+++ starting-code/app/scripts/modules/cart.js
@@ -1,9 +1,26 @@
 
-import products from 'modules/products';
+//jshint esversion: 6
+
+import { products, find } from 'modules/products';
 
 export default class Cart {
   constructor () {
     this.purchases = [];
+  }
+
+  // Add the product with the specified sku and quantity to the cart.
+  // sku must match one of the products, quantity must be > 0
+  // Returns a cart entry if successful, null otherwise
+  add(sku, quantity=1) {
+    let product = find(sku);
+    if (quantity < 0 || !product) return null;
+    let lineItem = { sku: (sku),
+      quantity: (quantity),
+      title: (product.title),
+      price: (product.price),
+      total: (quantity * product.price), };
+    this.purchases.push(lineItem);
+    return lineItem;
   }
 
   get length() {
@@ -12,5 +29,9 @@ export default class Cart {
 
   get total() {
     return 0;
+  }
+
+  get orders() {
+    return this.purchases;
   }
 }
--- starting-code/app/test/js/modules/cart-tests.js
+++ starting-code/app/test/js/modules/cart-tests.js
@@ -1,10 +1,20 @@
 // jshint esversion: 6
 import Cart from 'modules/cart';
 
-QUnit.module('Cart tests');
+QUnit.module('Cart');
 
-QUnit.test('new cart', assert => {
+QUnit.test('default values', assert => {
     const cart = new Cart();
     assert.equal(cart.length, 0, 'cart starts empty');
     assert.equal(cart.total, 0, 'no price');
   });
+
+  QUnit.test('adding a valid order', assert => {
+      const cart = new Cart();
+      let entry = cart.add('C10');
+      assert.equal(cart.length, 1, 'order count');
+      assert.equal(entry.sku, 'C10', 'sku');
+      assert.equal(entry.quantity, 1, 'quantity');
+      assert.ok(entry.title, 'title');
+      assert.ok(entry.price, 'price');
+    });
