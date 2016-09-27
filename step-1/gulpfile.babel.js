e845de29b30e0455c937fdbbd6f20a294c1c2786
Sarah Clark <sarahclark@google.com>
2016-09-27 12:17:20 -0700

    Disable the image cache (it was retaining old folder names)
    Change-Id: If35a46ed1e11e79a101762c2634671915123d171

--- step-1/gulpfile.babel.js
+++ step-1/gulpfile.babel.js
@@ -61,10 +61,10 @@ gulp.task('lint', () =>
 // Optimize images
 gulp.task('images', () =>
   gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
+    .pipe($.imagemin({ // DEBUG removed $.cache( before imagemin
       progressive: true,
       interlaced: true
-    })))
+    }))
     .pipe(gulp.dest('dist/images'))
     .pipe($.size({title: 'images'}))
 );
