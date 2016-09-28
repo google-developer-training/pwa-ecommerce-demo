c62939f699b1df6a081333b6a8ff1a7f509494c6
Sarah Clark <sarahclark@google.com>
2016-09-28 15:33:15 -0700

    Always serve from dist due to browersify change
    Change-Id: If55209d040023f5611db9967362bfaac3a501207

--- solution/gulpfile.babel.js
+++ solution/gulpfile.babel.js
@@ -195,30 +195,8 @@ gulp.task('test', ['transpile', 'copy test harness'], function() {
 });
 
 
-// Watch files for changes & reload
-gulp.task('serve', ['scripts', 'styles'], () => {
-  browserSync({
-    notify: false,
-    // Customize the Browsersync console logging prefix
-    logPrefix: 'WSK',
-    // Allow scroll syncing across breakpoints
-    scrollElementMapping: ['main', '.mdl-layout'],
-    // Run as an https by uncommenting 'https: true'
-    // Note: this uses an unsigned certificate which on first access
-    //       will present a certificate warning in the browser.
-    // https: true,
-    server: ['.tmp', 'app'],
-    port: 3000
-  });
-
-  gulp.watch(['app/**/*.html'], reload);
-  gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
-  gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts', reload]);
-  gulp.watch(['app/images/**/*'], reload);
-});
-
 // Build and serve the output from the dist build
-gulp.task('serve:dist', ['default'], () =>
+gulp.task('serve', ['default'], () =>
   browserSync({
     notify: false,
     logPrefix: 'WSK',
