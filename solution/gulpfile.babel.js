670719377131f959e941e8701f6db9025ef27da6
Sarah Clark <sarahclark@google.com>
2016-09-28 15:14:14 -0700

    Remove overly complex compilation step
    Change-Id: Ia834f3b05210693a5b37436b01d21b4ee7c589a6

--- solution/gulpfile.babel.js
+++ solution/gulpfile.babel.js
@@ -156,32 +156,6 @@ gulp.task('scripts', () => {
     .pipe(gulp.dest('dist/scripts/'));
 });
 
-// gulp.task('scripts', () =>
-//     // gulp.src([
-//     //   // Note: Since we are not using useref in the scripts build pipeline,
-//     //   //       you need to explicitly list your scripts here in the right order
-//     //   //       to be correctly concatenated
-//     //   './app/scripts/main.js',
-//     //   './app/scripts/cart.js'
-//     //   // Other scripts
-//     // ])
-//     browserify('./app/scripts/main.js', { debug: true })
-//       .transform(babelify, babelOptions)
-//       .bundle()
-//       .pipe(source('bundle.js'))
-//       .pipe($.newer('.tmp/scripts'))
-//       .pipe($.sourcemaps.init())
-//       .pipe($.babel())
-//       .pipe($.sourcemaps.write())
-//       .pipe(gulp.dest('.tmp/scripts'))
-//       .pipe($.concat('main.min.js'))
-//       .pipe($.uglify({preserveComments: 'some'}))
-//       // Output files
-//       .pipe($.size({title: 'scripts'}))
-//       .pipe($.sourcemaps.write('.'))
-//       .pipe(gulp.dest('dist/scripts'))
-// );
-
 // Scan your HTML for assets & optimize them
 gulp.task('html', () => {
   return gulp.src('app/**/*.html')
