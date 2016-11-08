3af0cfa3f1eb9bd8c87b51220a357db268a6e412
Sarah Clark <sarahclark@google.com>
2016-11-07 18:41:12 -0800

    Load cart on startup, added TODO items to app.js
    Change-Id: I13ac25daa8b3005e376053addbb0f59303a3df9f

--- solution/app/scripts/modules/app.js
+++ solution/app/scripts/modules/app.js
@@ -17,6 +17,7 @@ limitations under the License.
 //jshint esversion: 6
 
 import Cart from 'cart';
+import LocalStorage from 'local-storage';
 import CartView from 'cart-view';
 import ShopView from 'shop-view';
 import HeaderController from 'header-controller';
@@ -25,18 +26,23 @@ import {products} from 'products';
 export default class App {
 
   constructor() {
-    this._cart = new Cart();
+    this._storage = new LocalStorage();
+    this._cart = new Cart(this._storage);
     this._cartView = new CartView(this._cart);
     this._shop = new ShopView(this._cart);
     this._header = new HeaderController();
   }
 
   run() {
+    this._cart.load();
     this._shop.render();
     this._cartView.render();
-    // TODO set initial window.location
-    // TODO handle hashChange, manage history
-    // TODO manage element visibility
-    // TODO handle payment flow
+    // *** The following changes are meant to make this a single-page app ***
+    // TODO set initial window.location to index.html#store
+    // TODO handle hashChange, manage history (#store or #cart, maybe #pay)
+    // TODO manage element visibility (hide the cart when store is selected and vice versa)
+    // TODO merge payment dialog into index.html, handle payment flow
+    // TODO add a 'cart' event listener on document. All values of event.detail.action
+    // should trigger cart.save() EXCEPT event.detail.action==='load'
   }
 }
