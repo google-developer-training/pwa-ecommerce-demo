c64eec21ad3d850f01468a7c7edb5dd276dcb47b
Sarah Clark <sarahclark@google.com>
2016-11-18 15:44:39 -0800

    Add descriptions to all products
    Change-Id: I578fd4380216823102f8e105bb218a5fac81c8ac

--- solution/app/scripts/modules/products.js
+++ solution/app/scripts/modules/products.js
@@ -21,23 +21,23 @@ import Product from 'product';
 // Normally you would get these from a server
 export const products = [
   new Product('BarrelChair', 'Barrel Chair', 100.00, 'BarrelChair.jpg',
-    'A lovely chair made from upcycled barrel staves.'),
+    'A beautiful chair made of upcycled barrel staves'),
   new Product('C10', 'C10 Chair', 100.00, 'C10.jpg',
-    'PUT TEXT HERE'),
+    'A colorful chair with modern style and flair'),
   new Product('Cl2', 'CL2 Chair', 100.00, 'Cl2.jpg',
-    'PUT TEXT HERE'),
+    'A comfortable extended chair made for lounging'),
   new Product('CP03_blue', 'CP03 Chair', 100.00, 'CP03_blue.jpg',
-    'PUT TEXT HERE'),
+    'A wide cushion you can stack or move around the floor'),
   new Product('CPC_RECYCLED', 'CPC Upcycled', 100.00, 'CPC_RECYCLED.jpg',
-    'PUT TEXT HERE'),
+    'A simple chair with a seat of recycled plastic'),
   new Product('CPFS', 'CPFS', 100.00, 'CPFS.jpg',
-    'PUT TEXT HERE'),
+    'A footstool mader to complement the CPFS chair'),
   new Product('CPO2_red', 'CPO2', 100.00, 'CPO2_red.jpg',
-    'PUT TEXT HERE'),
+    'A narrow cushion, smaller than CP03, and quite comfortable'),
   new Product('CPT', 'CPT Table', 100.00, 'CPT.jpg',
-    'PUT TEXT HERE'),
+    'A perfect table for outdoors, has a hole for an umbrella'),
   new Product('CS1', 'CS1 Sofa', 100.00, 'CS1.jpg',
-    'PUT TEXT HERE'),
+    'A soft, stylish, and colorful sofa. Perfect for your home'),
 ];
 
 export function findProduct(sku, searchRange=products) {
