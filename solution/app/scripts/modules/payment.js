8d2ec81395a9df47fbba08849595d5bda7b5efca
Sarah Clark <sarahclark@sarahclark-macbookair.roam.corp.google.com>
2016-11-30 17:07:01 -0800

    Refactored for classroom use (more discrete steps)
    Change-Id: Icae6c9ae0a8911b9acc38fa85225c7588eb854e4

--- solution/app/scripts/modules/payment.js
+++ solution/app/scripts/modules/payment.js
@@ -13,112 +13,130 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-// jshint esversion: 6, sub:true
-import Cart from 'cart';
-import Product from 'product';
 
-/*
-* Given a cart set up with an order, gets payment authorization.
-* The display parameter gives a place to place the UI (could be
-* window or a div in the window.)
-* Returns a promise that resolves when payment is complete or rejects
-* (with a reason) when the payment fails.
-*/
-export default function processPayment(cart) {
-  const _shippingOptions = {
-    'standard': {
+const SHIPPING_OPTIONS = {
+  us: {
+    standard: {
       id: 'standard',
       label: 'Standard Shipping',
       price: 0
     },
-    'express': {
+    express: {
       id: 'express',
       label: 'Express Shipping',
       price: 10
-    },
-    'international': {
+    }
+  },
+  international: {
+    international: {
       id: 'international',
       label: 'International Shipping',
       price: 15
     }
-  };
+  }
+};
 
-  // convert `addr` into a dictionary
-  const toDict = function(addr) {
-    let dict = {};
-    if (addr) {
-      dict.country           = addr.country;
-      dict.region            = addr.region;
-      dict.city              = addr.city;
-      dict.dependentLocality = addr.dependentLocality;
-      dict.addressLine       = addr.addressLine;
-      dict.postalCode        = addr.postalCode;
-      dict.sortingCode       = addr.sortingCode;
-      dict.languageCode      = addr.languageCode;
-      dict.organization      = addr.organization;
-      dict.recipient         = addr.recipient;
-      dict.careOf            = addr.careOf;
-      dict.phone             = addr.phone;
-    }
-    return dict;
-  };
+const PAYMENT_METHODS = [
+  'visa', 'mastercard', 'amex', 'discover', 'maestro',
+  'diners', 'jcb', 'unionpay', 'bitcoin'
+];
 
-  const toShippingOption = function(option) {
-    return {
-      id: option.id,
-      label: option.label,
-      amount: {currency: 'USD', value: String(option.price)}
+export default class PaymentAPIWrapper {
+
+  /*
+  * Given a cart set up with an order, gets payment authorization.
+  * Returns a promise that resolves when payment is complete, this
+  * has a data object you can pass to the back-end payment server.
+  */
+  processPayment(cart) {
+    let request = this.buildPaymentRequest(cart);
+    // Show UI then continue with user payment info
+    return request.show().then(result => {
+      // Extract just the details we want to send to the server
+      var data = this.copy(result, 'methodName', 'details', 'payerEmail',
+        'payerPhone', 'shippingOption');
+      data.address = this.copy(result.shippingAddress, 'country', 'region',
+        'city', 'dependentLocality', 'addressLine', 'postalCode', 'sortingCode',
+        'languageCode', 'organization', 'recipient', 'careOf', 'phone');
+      return data;
+    });
+  }
+
+  /*
+   * Creates a PaymentRequest object including the event handlers used to
+   * update the payment details as the user makes choices.
+   */
+  buildPaymentRequest(cart) {
+    // Supported payment instruments
+    const supportedInstruments = [{
+      supportedMethods: (PAYMENT_METHODS)
+    }];
+
+    // Payment options
+    const paymentOptions = {
+      requestShipping: true,
+      requestPayerEmail: true,
+      requestPayerPhone: true
     };
-  };
 
-  let getPaymentDetails = function(cart, country, shippingOptionId) {
-    let displayItems = [];
-    let shippingOptions = [
-      toShippingOption(_shippingOptions['standard']),
-      toShippingOption(_shippingOptions['express'])
-    ];
-    let total = cart.total;
+    let shippingOptions = null;
+    let selectedOption = null;
 
-    for (let item of cart.cart) {
-      displayItems.push({
-        label: item.title + ' x ' + item.quantity,
+    let details = this.buildPaymentDetails(cart, shippingOptions, selectedOption);
+
+    // Initialize
+    let request = new window.PaymentRequest(supportedInstruments, details, paymentOptions);
+
+    // When user selects a shipping address, add shipping options to match
+    request.addEventListener('shippingaddresschange', e => {
+      e.updateWith(_ => {
+        // Get the shipping options and select the least expensive
+        shippingOptions = this.optionsForCountry(request.shippingAddress.country);
+        selectedOption = shippingOptions[0].id;
+        let details = this.buildPaymentDetails(cart, shippingOptions, selectedOption);
+        return Promise.resolve(details);
+      });
+    });
+
+    // When user selects a shipping option, update cost, etc. to match
+    request.addEventListener('shippingoptionchange', e => {
+      e.updateWith(_ => {
+        selectedOption = request.shippingOption;
+        let details = this.buildPaymentDetails(cart, shippingOptions, selectedOption);
+        return Promise.resolve(details);
+      });
+    });
+
+    return request;
+  }
+
+  /*
+   * Creates the PaymentDetails dictionary inside the PaymentRequest.
+   * This can change as the user selects shipping options.
+   */
+  buildPaymentDetails(cart, shippingOptions, shippingOptionId) {
+    // Start with the cart items
+    let displayItems = cart.cart.map(item => {
+      return {
+        label: `${item.quantity}x ${item.title}`,
         amount: {currency: 'USD', value: String(item.total)},
         selected: false
+      };
+    });
+    let total = cart.total;
+
+    let displayedShippingOptions = null;
+    if (shippingOptions) {
+      let selectedOption = shippingOptions[shippingOptionId];
+      displayedShippingOptions = shippingOptions.map(option => {
+        return {
+          id: option.id,
+          label: option.label,
+          amount: {currency: 'USD', value: String(option.price)},
+          selected: option === selectedOption
+        };
       });
-    }
-
-    // `country` is not specified on initialization
-    if (country) {
-      if (country === 'US') {
-        let option;
-        if (shippingOptionId == 'express') {
-          option = _shippingOptions['express'];
-          shippingOptions[1].selected = true;
-        } else {
-          // Treat `shippingOptionId` not being 'express' always as 'standard'
-          option = _shippingOptions['standard'];
-          shippingOptions[0].selected = true;
-        }
-        displayItems.push({
-          label: option.label,
-          amount: {currency: 'USD', value: String(option.price)}
-        });
-        total += option.price;
-      } else {
-        // Outside of US has only one shipping option
-        let option = _shippingOptions['international'];
-        // Overwrite shipping options
-        shippingOptions = [
-          toShippingOption(option)
-        ];
-        shippingOptions[0].selected = true;
-
-        displayItems.push({
-          label: option.label,
-          amount: {currency: 'USD', value: String(option.price)}
-        });
-        total += option.price;
-      }
+      total += selectedOption.price;
     }
 
     let details = {
@@ -127,86 +145,75 @@ export default function processPayment(cart) {
         label: 'Total due',
         amount: {currency: 'USD', value: String(total)}
       },
-      shippingOptions: shippingOptions
+      shippingOptions: displayedShippingOptions
     };
+
     return details;
-  };
+  }
 
-  // Supported payment instruments
-  let supportedInstruments = [{
-    supportedMethods: [
-      'visa', 'mastercard', 'amex', 'discover', 'maestro',
-      'diners', 'jcb', 'unionpay', 'bitcoin'
-    ]
-  }];
 
-  // Payment details
-  let details = getPaymentDetails(cart);
+  /**
+   * Utility function to extract fields from one object and copy them into a new
+   * object.
+   * @param {object} source copies from this object
+   * @param {...string} fields names of fields to copy
+   * @returns {object} new object with only the specified fields (copied from source)
+   */
+  copy(source, ...fields) {
+    if (source === null) return source;
+    let result = {};
+    for (let i = 0; i < fields.length; i++) {
+      const field = fields[i];
+      result[field] = source[field];
+    }
+    return result;
+  }
 
-  // Payment options
-  let options = {
-    requestShipping: true,
-    requestPayerEmail: true,
-    requestPayerPhone: true
-  };
-
-  // Initialize
-  let request = new PaymentRequest(supportedInstruments, details, options);
-
-  // When user selects a shipping address
-  request.addEventListener('shippingaddresschange', e => {
-    e.updateWith(((country) => {
-      let details = getPaymentDetails(cart, country);
-      return Promise.resolve(details);
-    })(request.shippingAddress.country));
-  });
-
-  // When user selects a shipping option
-  request.addEventListener('shippingoptionchange', e => {
-    e.updateWith(((country, shippingOption) => {
-      let details = getPaymentDetails(cart, country, shippingOption);
-      return Promise.resolve(details);
-    })(request.shippingAddress.country, request.shippingOption));
-  });
-
-  // Show UI then continue with user payment info
-  return request.show().then(result => {
-    // Manually clone the resulting object
-    var data = {};
-    data.methodName = result.methodName;
-    data.details    = result.details;
-    data.payerEmail = result.payerEmail;
-    data.payerPhone = result.payerPhone;
-    data.address    = toDict(result.shippingAddress);
-    data.shipping   = result.shippingOption;
-
-    // POST the object to the server
-    return fetch('/checkout.html', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    }).then(res => {
-      // Only if successful
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        throw 'Payment failure';
-      }
-    }).then(response => {
-      // You should have received a JSON object
-      if (response.success === true) {
-        result.complete('success');
-        return Promise.resolve();
-      } else {
-        result.complete('fail');
-        return Promise.reject();
-      }
-    }).catch(() => {
-      result.complete('fail');
-      return Promise.reject();
+  /*
+   * Utility function to extract the correct shipping options for a country.
+   */
+  optionsForCountry(country) {
+    if (!country || !SHIPPING_OPTIONS.hasOwnProperty(country)) {
+      country = 'international';
+    }
+    let options = SHIPPING_OPTIONS[country];
+    // Sort by price, lowest first
+    options.sort((a, b) => {
+      return a.price - b.price;
     });
-  });
+    return options;
+  }
+
 }
+
+/* Park this here until I find the right place for it
+ * // At this point, it's all specific to your payment processing system.
+       // We'll post to a URL on a sample server that ships with this example.
+       // It returns a JSON object with success == true.
+       // Of course, you would change this to match your back-end
+       return new Promise((resolve, reject) => {
+         fetch('/checkout.html', {
+           method: 'POST',
+           credentials: 'include',
+           headers: {
+             'Content-Type': 'application/json'
+           },
+           body: JSON.stringify(data)
+         }).then(res => {
+           if (res.status !== 200) {
+             throw new Error(`Payment failure (id ${res.status})`);
+           }
+           let json = res.json();
+           if (json.success === true) {
+             result.complete('success');
+             resolve();
+           } else {
+             result.complete('fail');
+             reject('!success');
+           }
+         }).catch(e => {
+           result.complete(`fail: ${e}`);
+           reject(e);
+         });
+       });
+ */
