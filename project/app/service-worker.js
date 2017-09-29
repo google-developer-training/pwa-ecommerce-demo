43f184274c5cbad769e49ea17d8257ca67d5e5d9
Marius Sebastian Trif <coolsebz@gmail.com>
2017-09-29 16:14:17 +0100

    Added SW Fetch with cache first strategy

--- project/app/service-worker.js
+++ project/app/service-worker.js
@@ -27,4 +27,35 @@ limitations under the License.
 
 (function() {
     console.log('Entering ServiceWorker script');
+
+
+    var staticCacheName = 'e-commerce-v1';
+
+
+    self.addEventListener('fetch', function(event) {
+        console.log('Fetch event for ', event.request.url);
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache');
+                    return response;
+                }
+                console.log('Network request for ', event.request.url);
+                return fetch(event.request).then(function(response) {
+                    if (response.status === 404) {
+                        return caches.match('pages/404.html');
+                    }
+                    return caches.open(staticCacheName).then(function(cache) {
+                        if (event.request.url.indexOf('test') < 0) {
+                            cache.put(event.request.url, response.clone());
+                        }
+                        return response;
+                    });
+                });
+            }).catch(function(error) {
+                console.log('Error, ', error);
+                return caches.match('pages/offline.html');
+            })
+        );
+    });
 })();
