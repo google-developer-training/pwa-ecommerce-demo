50871a6be741ddf8f8f5b6ecc765b2da7eb994bb
Nicolas Searle <nasearle@ucla.edu>
2018-04-18 09:48:45 -0700

    wrap sw reg in window.onload

--- solution/app/scripts/main.js
+++ solution/app/scripts/main.js
@@ -40,40 +40,42 @@ import {hasPrerequisites} from 'features';
 
   if ('serviceWorker' in navigator &&
       (window.location.protocol === 'https:' || isLocalhost)) {
-    navigator.serviceWorker.register('sw.js')
-    .then(registration => {
-      // updatefound is fired if service-worker.js changes.
-      registration.onupdatefound = () => {
-        // updatefound is also fired the very first time the SW is installed,
-        // and there's no need to prompt for a reload at that point.
-        // So check here to see if the page is already controlled,
-        // i.e. whether there's an existing service worker.
-        if (navigator.serviceWorker.controller) {
-          // The updatefound event implies that registration.installing is set:
-          // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-          const installingWorker = registration.installing;
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('sw.js')
+      .then(registration => {
+        // updatefound is fired if service-worker.js changes.
+        registration.onupdatefound = () => {
+          // updatefound is also fired the very first time the SW is installed,
+          // and there's no need to prompt for a reload at that point.
+          // So check here to see if the page is already controlled,
+          // i.e. whether there's an existing service worker.
+          if (navigator.serviceWorker.controller) {
+            // The updatefound event implies that registration.installing is set:
+            // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
+            const installingWorker = registration.installing;
 
-          installingWorker.onstatechange = () => {
-            switch (installingWorker.state) {
-              case 'installed':
-                // At this point, the old content will have been purged and the
-                // fresh content will have been added to the cache.
-                // It's the perfect time to display a "New content is
-                // available; please refresh." message in the page's interface.
-                break;
+            installingWorker.onstatechange = () => {
+              switch (installingWorker.state) {
+                case 'installed':
+                  // At this point, the fresh content will have been added to
+                  // the cache. It's the perfect time to display a "New content
+                  // is available; please refresh." message in the page's
+                  // interface.
+                  break;
 
-              case 'redundant':
-                throw new Error('The installing ' +
-                                'service worker became redundant.');
+                case 'redundant':
+                  throw new Error('The installing ' +
+                                  'service worker became redundant.');
 
-              default:
-                // Ignore
-            }
-          };
-        }
-      };
-    }).catch(e => {
-      console.error('Error during service worker registration:', e);
+                default:
+                  // Ignore
+              }
+            };
+          }
+        };
+      }).catch(e => {
+        console.error('Error during service worker registration:', e);
+      });
     });
   }
 
